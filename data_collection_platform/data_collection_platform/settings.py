77396241c1a216bbf8b97d9503733c4fc039ecf7
benjaminbridges <benjamin.bridges@crtrust.org>
2022-05-15 10:38:38 +0100

    adding the requirements file

--- data_collection_platform/data_collection_platform/settings.py
+++ data_collection_platform/data_collection_platform/settings.py
@@ -185,4 +185,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'benjaminbridges@gmail.com'
-EMAIL_HOST_PASSWORD = 'Nesheim2022'
+EMAIL_HOST_PASSWORD = ''
